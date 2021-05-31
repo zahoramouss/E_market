2041b71f36aba34edd7e7cdcd56aea7ffbb66244
alibarakat557 <76252317+alibarakat557@users.noreply.github.com>
2021-05-31 15:44:36 +0300

    create resores

---
+++ reserce.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color blac = Color(0xff3a3939);
+const Color agray = Color(0xffb5bdd3);
+const Color ongo = Color(0xffff6856);
+const Color wite = Color(0xffffffff);
+const Color bule = Color(0xff4076e3);
+const Color dark = Color(0xff70a9ff);
